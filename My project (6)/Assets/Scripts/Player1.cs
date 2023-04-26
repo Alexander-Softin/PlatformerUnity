cf01130d8d678beaff13479554488297f36ed215
champashan <71319975+champashan@users.noreply.github.com>
2023-04-26 17:34:42 +0300

    commit bogdan animation
    анимация вроде как готова ;)

--- My project (6)/Assets/Scripts/Player1.cs
+++ My project (6)/Assets/Scripts/Player1.cs
@@ -8,10 +8,15 @@ public class Player1 : MonoBehaviour
     
     [SerializeField] private float speed;//открытый для редакции в unity
     private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
+
 
     private void Awake()
     {
+        //эти две строки помогут получить ссылки для нашего rigidbody и animator 
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -21,12 +26,35 @@ public class Player1 : MonoBehaviour
         //в input manager я настроил horizontal И horizontal2 для двух игроков как раз на одной клаве
 
         //управление стрелками
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        //поворот игрока при движении вправо/влево
+
+        if (horizontalInput > 0.01f)
+            transform.localScale = new Vector3(6, 6, 6);
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-6, 6, 6);
 
         //прыжок
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            body.velocity = new Vector2(body.velocity.x, speed);
-        }
+        if (Input.GetKey(KeyCode.UpArrow) && grounded)
+            Jump();
+
+        //параметры анимации
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
--- My project (6)/Assets/Scripts/Player2.cs
+++ My project (6)/Assets/Scripts/Player2.cs
@@ -9,24 +9,49 @@ public class Player2 : MonoBehaviour
 
     [SerializeField] private float speed;//открытый для редакции в unity
     private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
 
     private void Awake()
     {
+        //эти две строки помогут получить ссылки для нашего rigidbody и animator 
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
         //как ты можешь заметить
         //здесь уже horizontal2 и перс
-       
+
         //управляется на клавиши a d
-        body.velocity = new Vector2(Input.GetAxis("Horizontal2") * speed, body.velocity.y);
+
+        float horizontalInput = Input.GetAxis("Horizontal2");
+
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        if (horizontalInput > 0.01f)
+            transform.localScale = new Vector3(4, 4, 4);
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-4, 4, 4);
 
         //прыжок
-        if (Input.GetKey(KeyCode.W))
-        {
-            body.velocity = new Vector2(body.velocity.x, speed);
-        }
+        if (Input.GetKey(KeyCode.W) && grounded)
+            Jump();
+        
+
+        anim.SetBool("run2", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+    }
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump2");
+        grounded = false;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
